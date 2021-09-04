7495bd50c3dfa3ba93c85e51720c3348dbc1a1f2
Seeloewen <Louis.Herbert2006@gmail.com>
2021-09-04 16:52:48 +0200

    Created first start window to select language

---
+++ Seeloewen Shutdown/frmFirstStart.vb
@@ -0,0 +1,16 @@
+﻿Public Class frmFirstStart
+    Private Sub frmFirstStart_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbxLanguage.SelectedItem = "English (English)"
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        If cbxLanguage.SelectedItem = "English (English)" Then
+            My.Settings.Language = "English"
+            Close()
+        ElseIf cbxLanguage.SelectedItem = "Deutsch (German)" Then
+            My.Settings.Language = "German"
+            Close()
+        Else msgbox("Please pick a Language!")
+        End If
+    End Sub
+End Class
