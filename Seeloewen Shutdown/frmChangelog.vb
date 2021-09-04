3610dc90e624c9709f93600bf2cfded077743181
Seeloewen <Louis.Herbert2006@gmail.com>
2021-09-04 16:52:10 +0200

    Added english translation

--- Seeloewen Shutdown/frmChangelog.vb
+++ Seeloewen Shutdown/frmChangelog.vb
@@ -2,4 +2,10 @@
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Close()
     End Sub
+
+    Private Sub frmChangelog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.Language = "English" Then
+            lblChangelogDescription.Text = "In the changelog you can easily find news for different versions."
+        End If
+    End Sub
 End Class
