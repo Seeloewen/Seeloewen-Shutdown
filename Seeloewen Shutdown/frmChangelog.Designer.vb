c7cbeea6f1cf6364249b1f7301cdd18528522c0f
Seeloewen <Louis.Herbert2006@gmail.com>
2021-10-23 23:44:35 +0200

    Added news for 1.5.0

--- Seeloewen Shutdown/frmChangelog.Designer.vb
+++ Seeloewen Shutdown/frmChangelog.Designer.vb
@@ -96,7 +96,7 @@ Partial Class frmChangelog
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.CancelButton = Me.btnClose
-        Me.ClientSize = New System.Drawing.Size(518, 343)
+        Me.ClientSize = New System.Drawing.Size(518, 341)
         Me.Controls.Add(Me.rtbchangelogEnglish)
         Me.Controls.Add(Me.gbChangelog)
         Me.Controls.Add(Me.btnClose)
