01f938c71cb7fff821683d111dd83d7214a1b5f5
Seeloewen <Louis.Herbert2006@gmail.com>
2021-11-05 15:07:53 +0100

    Add Updater Settings (Incomplete)

---
+++ Seeloewen Shutdown/frmUpdaterSettings.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUpdaterSettings
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUpdaterSettings = New System.Windows.Forms.Label()
+        Me.rbtnDownloadNewestUpdater = New System.Windows.Forms.RadioButton()
+        Me.rbtnUseLegacyUpdater = New System.Windows.Forms.RadioButton()
+        Me.rbtnUseCustomUpdater = New System.Windows.Forms.RadioButton()
+        Me.tbCustomUpdater = New System.Windows.Forms.TextBox()
+        Me.tbNewestUpdaterLink = New System.Windows.Forms.TextBox()
+        Me.btnUseDefaultUpdater = New System.Windows.Forms.Button()
+        Me.rbtnUseCurrentSettingsForUpdater = New System.Windows.Forms.RadioButton()
+        Me.rbtnCustomSettings = New System.Windows.Forms.RadioButton()
+        Me.lblDesign = New System.Windows.Forms.Label()
+        Me.lblLanguage = New System.Windows.Forms.Label()
+        Me.cbxLanguage = New System.Windows.Forms.ComboBox()
+        Me.cbxDesign = New System.Windows.Forms.ComboBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnBrowseCustomUpdater = New System.Windows.Forms.Button()
+        Me.gbUpdaterSettings = New System.Windows.Forms.GroupBox()
+        Me.gbUpdater = New System.Windows.Forms.GroupBox()
+        Me.gbUpdaterSettings.SuspendLayout()
+        Me.gbUpdater.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblUpdaterSettings
+        '
+        Me.lblUpdaterSettings.AutoSize = True
+        Me.lblUpdaterSettings.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUpdaterSettings.Location = New System.Drawing.Point(13, 13)
+        Me.lblUpdaterSettings.Name = "lblUpdaterSettings"
+        Me.lblUpdaterSettings.Size = New System.Drawing.Size(146, 20)
+        Me.lblUpdaterSettings.TabIndex = 0
+        Me.lblUpdaterSettings.Text = "Updater Settings"
+        '
+        'rbtnDownloadNewestUpdater
+        '
+        Me.rbtnDownloadNewestUpdater.AutoSize = True
+        Me.rbtnDownloadNewestUpdater.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnDownloadNewestUpdater.Location = New System.Drawing.Point(15, 19)
+        Me.rbtnDownloadNewestUpdater.Name = "rbtnDownloadNewestUpdater"
+        Me.rbtnDownloadNewestUpdater.Size = New System.Drawing.Size(374, 20)
+        Me.rbtnDownloadNewestUpdater.TabIndex = 1
+        Me.rbtnDownloadNewestUpdater.TabStop = True
+        Me.rbtnDownloadNewestUpdater.Text = "Neueste Version des Updaters automatisch herunterladen:"
+        Me.rbtnDownloadNewestUpdater.UseVisualStyleBackColor = True
+        '
+        'rbtnUseLegacyUpdater
+        '
+        Me.rbtnUseLegacyUpdater.AutoSize = True
+        Me.rbtnUseLegacyUpdater.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnUseLegacyUpdater.Location = New System.Drawing.Point(15, 71)
+        Me.rbtnUseLegacyUpdater.Name = "rbtnUseLegacyUpdater"
+        Me.rbtnUseLegacyUpdater.Size = New System.Drawing.Size(193, 20)
+        Me.rbtnUseLegacyUpdater.TabIndex = 2
+        Me.rbtnUseLegacyUpdater.TabStop = True
+        Me.rbtnUseLegacyUpdater.Text = "Legacy-Updater verwenden"
+        Me.rbtnUseLegacyUpdater.UseVisualStyleBackColor = True
+        '
+        'rbtnUseCustomUpdater
+        '
+        Me.rbtnUseCustomUpdater.AutoSize = True
+        Me.rbtnUseCustomUpdater.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnUseCustomUpdater.Location = New System.Drawing.Point(15, 97)
+        Me.rbtnUseCustomUpdater.Name = "rbtnUseCustomUpdater"
+        Me.rbtnUseCustomUpdater.Size = New System.Drawing.Size(264, 20)
+        Me.rbtnUseCustomUpdater.TabIndex = 3
+        Me.rbtnUseCustomUpdater.TabStop = True
+        Me.rbtnUseCustomUpdater.Text = "Benutzerdefinierten Updater verwenden:"
+        Me.rbtnUseCustomUpdater.UseVisualStyleBackColor = True
+        '
+        'tbCustomUpdater
+        '
+        Me.tbCustomUpdater.Location = New System.Drawing.Point(15, 123)
+        Me.tbCustomUpdater.Name = "tbCustomUpdater"
+        Me.tbCustomUpdater.Size = New System.Drawing.Size(354, 20)
+        Me.tbCustomUpdater.TabIndex = 4
+        '
+        'tbNewestUpdaterLink
+        '
+        Me.tbNewestUpdaterLink.Location = New System.Drawing.Point(15, 45)
+        Me.tbNewestUpdaterLink.Name = "tbNewestUpdaterLink"
+        Me.tbNewestUpdaterLink.Size = New System.Drawing.Size(354, 20)
+        Me.tbNewestUpdaterLink.TabIndex = 5
+        Me.tbNewestUpdaterLink.Text = "https://github.com/Seeloewen/Seeloewen-Shutdown-Update/blob/main/Latest%20Build/S" &
+    "eeloewen-Shutdown-Update.exe?raw=true"
+        '
+        'btnUseDefaultUpdater
+        '
+        Me.btnUseDefaultUpdater.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUseDefaultUpdater.Location = New System.Drawing.Point(375, 43)
+        Me.btnUseDefaultUpdater.Name = "btnUseDefaultUpdater"
+        Me.btnUseDefaultUpdater.Size = New System.Drawing.Size(93, 23)
+        Me.btnUseDefaultUpdater.TabIndex = 6
+        Me.btnUseDefaultUpdater.Text = "Standard"
+        Me.btnUseDefaultUpdater.UseVisualStyleBackColor = True
+        '
+        'rbtnUseCurrentSettingsForUpdater
+        '
+        Me.rbtnUseCurrentSettingsForUpdater.AutoSize = True
+        Me.rbtnUseCurrentSettingsForUpdater.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnUseCurrentSettingsForUpdater.Location = New System.Drawing.Point(15, 19)
+        Me.rbtnUseCurrentSettingsForUpdater.Name = "rbtnUseCurrentSettingsForUpdater"
+        Me.rbtnUseCurrentSettingsForUpdater.Size = New System.Drawing.Size(381, 20)
+        Me.rbtnUseCurrentSettingsForUpdater.TabIndex = 7
+        Me.rbtnUseCurrentSettingsForUpdater.TabStop = True
+        Me.rbtnUseCurrentSettingsForUpdater.Text = "Aktuelle Einstellungen der App für den Updater übernehmen"
+        Me.rbtnUseCurrentSettingsForUpdater.UseVisualStyleBackColor = True
+        '
+        'rbtnCustomSettings
+        '
+        Me.rbtnCustomSettings.AutoSize = True
+        Me.rbtnCustomSettings.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbtnCustomSettings.Location = New System.Drawing.Point(15, 45)
+        Me.rbtnCustomSettings.Name = "rbtnCustomSettings"
+        Me.rbtnCustomSettings.Size = New System.Drawing.Size(219, 20)
+        Me.rbtnCustomSettings.TabIndex = 8
+        Me.rbtnCustomSettings.TabStop = True
+        Me.rbtnCustomSettings.Text = "Benutzerdefinierte Einstellungen:"
+        Me.rbtnCustomSettings.UseVisualStyleBackColor = True
+        '
+        'lblDesign
+        '
+        Me.lblDesign.AutoSize = True
+        Me.lblDesign.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDesign.Location = New System.Drawing.Point(34, 99)
+        Me.lblDesign.Name = "lblDesign"
+        Me.lblDesign.Size = New System.Drawing.Size(51, 16)
+        Me.lblDesign.TabIndex = 9
+        Me.lblDesign.Text = "Design"
+        '
+        'lblLanguage
+        '
+        Me.lblLanguage.AutoSize = True
+        Me.lblLanguage.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLanguage.Location = New System.Drawing.Point(34, 72)
+        Me.lblLanguage.Name = "lblLanguage"
+        Me.lblLanguage.Size = New System.Drawing.Size(59, 16)
+        Me.lblLanguage.TabIndex = 10
+        Me.lblLanguage.Text = "Sprache"
+        '
+        'cbxLanguage
+        '
+        Me.cbxLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxLanguage.FormattingEnabled = True
+        Me.cbxLanguage.Location = New System.Drawing.Point(99, 71)
+        Me.cbxLanguage.Name = "cbxLanguage"
+        Me.cbxLanguage.Size = New System.Drawing.Size(145, 21)
+        Me.cbxLanguage.TabIndex = 12
+        '
+        'cbxDesign
+        '
+        Me.cbxDesign.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxDesign.FormattingEnabled = True
+        Me.cbxDesign.Location = New System.Drawing.Point(99, 98)
+        Me.cbxDesign.Name = "cbxDesign"
+        Me.cbxDesign.Size = New System.Drawing.Size(145, 21)
+        Me.cbxDesign.TabIndex = 13
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(267, 339)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(237, 23)
+        Me.btnClose.TabIndex = 14
+        Me.btnClose.Text = "Speichern"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(17, 339)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(244, 23)
+        Me.btnCancel.TabIndex = 15
+        Me.btnCancel.Text = "Abbrechen"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnBrowseCustomUpdater
+        '
+        Me.btnBrowseCustomUpdater.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBrowseCustomUpdater.Location = New System.Drawing.Point(375, 121)
+        Me.btnBrowseCustomUpdater.Name = "btnBrowseCustomUpdater"
+        Me.btnBrowseCustomUpdater.Size = New System.Drawing.Size(95, 23)
+        Me.btnBrowseCustomUpdater.TabIndex = 16
+        Me.btnBrowseCustomUpdater.Text = "Durchsuchen"
+        Me.btnBrowseCustomUpdater.UseVisualStyleBackColor = True
+        '
+        'gbUpdaterSettings
+        '
+        Me.gbUpdaterSettings.Controls.Add(Me.rbtnUseCurrentSettingsForUpdater)
+        Me.gbUpdaterSettings.Controls.Add(Me.rbtnCustomSettings)
+        Me.gbUpdaterSettings.Controls.Add(Me.cbxLanguage)
+        Me.gbUpdaterSettings.Controls.Add(Me.cbxDesign)
+        Me.gbUpdaterSettings.Controls.Add(Me.lblLanguage)
+        Me.gbUpdaterSettings.Controls.Add(Me.lblDesign)
+        Me.gbUpdaterSettings.Location = New System.Drawing.Point(17, 199)
+        Me.gbUpdaterSettings.Name = "gbUpdaterSettings"
+        Me.gbUpdaterSettings.Size = New System.Drawing.Size(487, 132)
+        Me.gbUpdaterSettings.TabIndex = 17
+        Me.gbUpdaterSettings.TabStop = False
+        '
+        'gbUpdater
+        '
+        Me.gbUpdater.Controls.Add(Me.rbtnDownloadNewestUpdater)
+        Me.gbUpdater.Controls.Add(Me.rbtnUseLegacyUpdater)
+        Me.gbUpdater.Controls.Add(Me.btnBrowseCustomUpdater)
+        Me.gbUpdater.Controls.Add(Me.rbtnUseCustomUpdater)
+        Me.gbUpdater.Controls.Add(Me.btnUseDefaultUpdater)
+        Me.gbUpdater.Controls.Add(Me.tbCustomUpdater)
+        Me.gbUpdater.Controls.Add(Me.tbNewestUpdaterLink)
+        Me.gbUpdater.Location = New System.Drawing.Point(17, 36)
+        Me.gbUpdater.Name = "gbUpdater"
+        Me.gbUpdater.Size = New System.Drawing.Size(487, 157)
+        Me.gbUpdater.TabIndex = 18
+        Me.gbUpdater.TabStop = False
+        '
+        'frmUpdaterSettings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(519, 370)
+        Me.Controls.Add(Me.gbUpdater)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.lblUpdaterSettings)
+        Me.Controls.Add(Me.gbUpdaterSettings)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmUpdaterSettings"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Updater Settings"
+        Me.gbUpdaterSettings.ResumeLayout(False)
+        Me.gbUpdaterSettings.PerformLayout()
+        Me.gbUpdater.ResumeLayout(False)
+        Me.gbUpdater.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUpdaterSettings As Label
+    Friend WithEvents rbtnDownloadNewestUpdater As RadioButton
+    Friend WithEvents rbtnUseLegacyUpdater As RadioButton
+    Friend WithEvents rbtnUseCustomUpdater As RadioButton
+    Friend WithEvents tbCustomUpdater As TextBox
+    Friend WithEvents tbNewestUpdaterLink As TextBox
+    Friend WithEvents btnUseDefaultUpdater As Button
+    Friend WithEvents rbtnUseCurrentSettingsForUpdater As RadioButton
+    Friend WithEvents rbtnCustomSettings As RadioButton
+    Friend WithEvents lblDesign As Label
+    Friend WithEvents lblLanguage As Label
+    Friend WithEvents cbxLanguage As ComboBox
+    Friend WithEvents cbxDesign As ComboBox
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnBrowseCustomUpdater As Button
+    Friend WithEvents gbUpdaterSettings As GroupBox
+    Friend WithEvents gbUpdater As GroupBox
+End Class
---
+++ Seeloewen Shutdown/frmUpdaterSettings.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmUpdaterSettings
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        'Save Settings
+
+        'Close
+        Close()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        'Show Dialog, that clicking "Cancel" doesn't save options
+        Select Case MsgBox("Möchtest du die Updater-Einstellungen wirklich schließen ohne Änderungen zu speichern?", MsgBoxStyle.YesNo, "Einstellungen verwerfen")
+            Case Windows.Forms.DialogResult.Yes
+                Me.Close()
+            Case Windows.Forms.DialogResult.No
+        End Select
+    End Sub
+
+    Private Sub btnUseDefaultUpdater_Click(sender As Object, e As EventArgs) Handles btnUseDefaultUpdater.Click
+        tbNewestUpdaterLink.Text = "https://github.com/Seeloewen/Seeloewen-Shutdown-Update/blob/main/Latest%20Build/Seeloewen-Shutdown-Update.exe?raw=true"
+    End Sub
+End Class
