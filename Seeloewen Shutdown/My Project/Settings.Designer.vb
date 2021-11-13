9b2e94e3aa6d853c576525326410576fc28dcd3b
Seeloewen <Louis.Herbert2006@gmail.com>
2021-11-13 17:33:12 +0100

    Add UpdaterBranch setting

--- Seeloewen Shutdown/My Project/Settings.Designer.vb
+++ Seeloewen Shutdown/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -220,6 +220,18 @@ Namespace My
             End Get
             Set
                 Me("CustomUpdaterDesign") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("Release")>  _
+        Public Property UpdaterBranch() As String
+            Get
+                Return CType(Me("UpdaterBranch"),String)
+            End Get
+            Set
+                Me("UpdaterBranch") = value
             End Set
         End Property
     End Class
