0abc98d6b61363368e175cd40f5cb51808f16b7a
Seeloewen <Louis.Herbert2006@gmail.com>
2022-01-28 22:27:31 +0100

    Update btnSettings, add About

--- Seeloewen Shutdown/My Project/Resources.Designer.vb
+++ Seeloewen Shutdown/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property About() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("About", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property btnHamburger() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("btnHamburger", resourceCulture)
