24c518957893e899e737cf9e8c2a2d3f40765ed4
Seeloewen <Louis.Herbert2006@gmail.com>
2022-03-06 19:28:02 +0100

    Add settings for Last Action

--- Seeloewen Shutdown/My Project/Settings.Designer.vb
+++ Seeloewen Shutdown/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -135,6 +135,78 @@ Namespace My
             End Get
             Set
                 Me("Design") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property LastAction() As String
+            Get
+                Return CType(Me("LastAction"),String)
+            End Get
+            Set
+                Me("LastAction") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property LastTime() As String
+            Get
+                Return CType(Me("LastTime"),String)
+            End Get
+            Set
+                Me("LastTime") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property LastDate() As String
+            Get
+                Return CType(Me("LastDate"),String)
+            End Get
+            Set
+                Me("LastDate") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property LastActionDisplay() As String
+            Get
+                Return CType(Me("LastActionDisplay"),String)
+            End Get
+            Set
+                Me("LastActionDisplay") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property LastTimeDisplay() As String
+            Get
+                Return CType(Me("LastTimeDisplay"),String)
+            End Get
+            Set
+                Me("LastTimeDisplay") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property LastDateDisplay() As String
+            Get
+                Return CType(Me("LastDateDisplay"),String)
+            End Get
+            Set
+                Me("LastDateDisplay") = value
             End Set
         End Property
     End Class
