d2d574f3a7a7afb1acd7a90a07a937e20ca68545
Seeloewen <Louis.Herbert2006@gmail.com>
2022-05-14 00:03:43 +0200

    Add minimalistic view

---
+++ Seeloewen Shutdown/frmMinimalisticView.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMinimalisticView
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMinimalisticView))
+        Me.lblAction = New System.Windows.Forms.Label()
+        Me.lblTimerShutdown = New System.Windows.Forms.Label()
+        Me.tmrShutdown = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTimeRemaining = New System.Windows.Forms.Label()
+        Me.btnSwitchToNormalView = New System.Windows.Forms.Button()
+        Me.lblScheduledTime = New System.Windows.Forms.Label()
+        Me.lblTime = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblAction
+        '
+        Me.lblAction.AutoSize = True
+        Me.lblAction.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAction.Location = New System.Drawing.Point(13, 13)
+        Me.lblAction.Name = "lblAction"
+        Me.lblAction.Size = New System.Drawing.Size(291, 24)
+        Me.lblAction.TabIndex = 0
+        Me.lblAction.Text = "A %action% has been scheduled!"
+        '
+        'lblTimerShutdown
+        '
+        Me.lblTimerShutdown.AutoSize = True
+        Me.lblTimerShutdown.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimerShutdown.Location = New System.Drawing.Point(149, 78)
+        Me.lblTimerShutdown.Name = "lblTimerShutdown"
+        Me.lblTimerShutdown.Size = New System.Drawing.Size(76, 20)
+        Me.lblTimerShutdown.TabIndex = 1
+        Me.lblTimerShutdown.Text = "%Timer%"
+        '
+        'tmrShutdown
+        '
+        '
+        'lblTimeRemaining
+        '
+        Me.lblTimeRemaining.AutoSize = True
+        Me.lblTimeRemaining.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimeRemaining.Location = New System.Drawing.Point(13, 78)
+        Me.lblTimeRemaining.Name = "lblTimeRemaining"
+        Me.lblTimeRemaining.Size = New System.Drawing.Size(120, 20)
+        Me.lblTimeRemaining.TabIndex = 2
+        Me.lblTimeRemaining.Text = "Time remaining:"
+        '
+        'btnSwitchToNormalView
+        '
+        Me.btnSwitchToNormalView.BackgroundImage = Global.Seeloewen_Shutdown.My.Resources.Resources.button
+        Me.btnSwitchToNormalView.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnSwitchToNormalView.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.btnSwitchToNormalView.FlatAppearance.BorderSize = 0
+        Me.btnSwitchToNormalView.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnSwitchToNormalView.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnSwitchToNormalView.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSwitchToNormalView.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSwitchToNormalView.ForeColor = System.Drawing.Color.White
+        Me.btnSwitchToNormalView.Location = New System.Drawing.Point(132, 116)
+        Me.btnSwitchToNormalView.Name = "btnSwitchToNormalView"
+        Me.btnSwitchToNormalView.Size = New System.Drawing.Size(160, 28)
+        Me.btnSwitchToNormalView.TabIndex = 3
+        Me.btnSwitchToNormalView.Text = "Switch to Normal View"
+        Me.btnSwitchToNormalView.UseVisualStyleBackColor = True
+        '
+        'lblScheduledTime
+        '
+        Me.lblScheduledTime.AutoSize = True
+        Me.lblScheduledTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScheduledTime.Location = New System.Drawing.Point(13, 50)
+        Me.lblScheduledTime.Name = "lblScheduledTime"
+        Me.lblScheduledTime.Size = New System.Drawing.Size(123, 20)
+        Me.lblScheduledTime.TabIndex = 4
+        Me.lblScheduledTime.Text = "Scheduled time:"
+        '
+        'lblTime
+        '
+        Me.lblTime.AutoSize = True
+        Me.lblTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTime.Location = New System.Drawing.Point(149, 50)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(143, 20)
+        Me.lblTime.TabIndex = 5
+        Me.lblTime.Text = "%ShutdownTime%"
+        '
+        'frmMinimalisticView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(425, 156)
+        Me.Controls.Add(Me.lblTime)
+        Me.Controls.Add(Me.lblScheduledTime)
+        Me.Controls.Add(Me.btnSwitchToNormalView)
+        Me.Controls.Add(Me.lblTimeRemaining)
+        Me.Controls.Add(Me.lblTimerShutdown)
+        Me.Controls.Add(Me.lblAction)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmMinimalisticView"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Seeloewen Shutdown - Minimalistic View"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAction As Label
+    Friend WithEvents lblTimerShutdown As Label
+    Friend WithEvents tmrShutdown As Timer
+    Friend WithEvents lblTimeRemaining As Label
+    Friend WithEvents btnSwitchToNormalView As Button
+    Friend WithEvents lblScheduledTime As Label
+    Friend WithEvents lblTime As Label
+End Class
---
+++ Seeloewen Shutdown/frmMinimalisticView.vb
@@ -0,0 +1,81 @@
+﻿Public Class frmMinimalisticView
+
+    Dim TargetDT As DateTime
+
+    Private Sub tmrShutdown_Tick(sender As Object, e As EventArgs) Handles tmrShutdown.Tick
+        Dim ts As TimeSpan = TargetDT.Subtract(DateTime.Now)
+        If ts.TotalMilliseconds > 0 Then
+            If ts.TotalHours > 24 Then
+                lblTimerShutdown.Text = ts.ToString("dd\:hh\:mm\:ss")
+            Else lblTimerShutdown.Text = ts.ToString("hh\:mm\:ss")
+            End If
+        End If
+    End Sub
+
+
+    Private Sub frmMinimalisticView_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Load action
+        If frmMain._RunningAction.Text = "Shutdown" Or frmMain._RunningAction.Text = "Herunterfahren" Then
+            If My.Settings.Language = "German" Then
+                lblAction.Text = "Ein Herunterfahren wurde geplant!"
+            ElseIf My.Settings.Language = "English" Then
+                lblAction.Text = "A shutdown has been scheduled!"
+            End If
+        ElseIf frmMain._RunningAction.Text = "Restart" Or frmMain._RunningAction.Text = "Neustarten" Then
+            If My.Settings.Language = "German" Then
+                lblAction.Text = "Ein Neustart wurde geplant!"
+            ElseIf My.Settings.Language = "English" Then
+                lblAction.Text = "A restart has been scheduled!"
+            End If
+        End If
+
+        'Load time
+        lblTime.Text = frmMain._RunningTime.Text
+
+        'Load timer
+        TargetDT = DateTime.Now.AddSeconds(frmMain.ts.TotalSeconds)
+        tmrShutdown.Enabled = True
+
+        'Load design
+        If My.Settings.Design = "Dark" Then
+            lblAction.ForeColor = Color.White
+            lblScheduledTime.ForeColor = Color.White
+            lblTime.ForeColor = Color.White
+            lblTimeRemaining.ForeColor = Color.White
+            lblTimerShutdown.ForeColor = Color.White
+            BackColor = Color.FromArgb(41, 41, 41)
+        End If
+
+        'Load language
+        If My.Settings.Language = "German" Then
+            lblScheduledTime.Text = "Geplante Zeit:"
+            lblTimeRemaining.Text = "Verbleibende Zeit:"
+            Text = "Seeloewen Shutdown - Minimalistische Ansicht"
+            btnSwitchToNormalView.Text = "Zur Normalen Ansicht wechseln"
+        End If
+
+        'Hide frmMain in case it wasn't hidden when enabling minimalistic view
+        frmMain.Hide()
+    End Sub
+
+    Private Sub btnSwitchToNormalView_Click(sender As Object, e As EventArgs) Handles btnSwitchToNormalView.Click
+        frmMain.Show()
+        Close()
+    End Sub
+
+    Private Sub btnSwitchToNormalView_MouseDown(sender As Object, e As MouseEventArgs) Handles btnSwitchToNormalView.MouseDown
+        btnSwitchToNormalView.BackgroundImage = My.Resources.button_click
+    End Sub
+
+    Private Sub btnSwitchToNormalView_MouseHover(sender As Object, e As EventArgs) Handles btnSwitchToNormalView.MouseHover
+        btnSwitchToNormalView.BackgroundImage = My.Resources.button_hover
+    End Sub
+
+    Private Sub btnSwitchToNormalView_MouseLeave(sender As Object, e As EventArgs) Handles btnSwitchToNormalView.MouseLeave
+        btnSwitchToNormalView.BackgroundImage = My.Resources.button
+    End Sub
+
+    Private Sub btnSwitchToNormalView_MouseUp(sender As Object, e As MouseEventArgs) Handles btnSwitchToNormalView.MouseUp
+        btnSwitchToNormalView.BackgroundImage = My.Resources.button
+    End Sub
+End Class
