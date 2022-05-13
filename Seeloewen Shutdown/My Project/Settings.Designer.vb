ce0134b1b80c8a6fe64c5f7a154dab70b4c6e1e9
Seeloewen <Louis.Herbert2006@gmail.com>
2022-05-14 00:05:25 +0200

    Add settings for minimalistic view and notifications

--- Seeloewen Shutdown/My Project/Settings.Designer.vb
+++ Seeloewen Shutdown/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -183,6 +183,30 @@ Namespace My
             End Get
             Set
                 Me("LastDateDisplay") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+        Public Property EnableMinimalisticView() As Boolean
+            Get
+                Return CType(Me("EnableMinimalisticView"),Boolean)
+            End Get
+            Set
+                Me("EnableMinimalisticView") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+        Public Property ShowNotifications() As Boolean
+            Get
+                Return CType(Me("ShowNotifications"),Boolean)
+            End Get
+            Set
+                Me("ShowNotifications") = value
             End Set
         End Property
     End Class
