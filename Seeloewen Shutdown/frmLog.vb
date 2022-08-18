9c1e94cf44ac2e993712318a2f9169e11a037c89
Seeloewen <Louis.Herbert2006@gmail.com>
2022-08-18 04:23:20 +0200

    Add comments; Clean up code (frmLog)

--- Seeloewen Shutdown/frmLog.vb
+++ Seeloewen Shutdown/frmLog.vb
@@ -1,5 +1,13 @@
 ﻿Public Class frmLog
 
+    '-- Event handlers --
+
+    Private Sub frmLog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDesign()
+        LoadLanguage()
+        rtbLog.LoadFile(frmMain.AppData + "/Seeloewen Shutdown/DebugLogTemp") 'Loads log from file
+    End Sub
+
     Private Sub btnSaveAsFile_Click(sender As Object, e As EventArgs) Handles btnSaveAsFile.Click
         sfdLog.ShowDialog()
     End Sub
@@ -8,7 +16,7 @@
         My.Computer.FileSystem.WriteAllText(sfdLog.FileName, rtbLog.Text, False)
     End Sub
 
-    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click 'Clears log in both this window and main window
         rtbLog.Clear()
         frmMain.rtbLog.Clear()
     End Sub
@@ -16,6 +24,27 @@
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Close()
     End Sub
+
+    '-- Custom methods --
+
+    Private Sub LoadDesign()
+        If My.Settings.Design = "Dark" Then
+            BackColor = Color.FromArgb(41, 41, 41)
+            lblHeader.ForeColor = Color.White
+            rtbLog.BackColor = Color.FromArgb(41, 41, 41)
+        End If
+    End Sub
+
+    Private Sub LoadLanguage()
+        If My.Settings.Language = "German" Then
+            sfdLog.Title = "Speichere Log als Datei..."
+            btnSaveAsFile.Text = "Als Datei speichern"
+            btnClear.Text = "Leeren"
+            btnClose.Text = "Schließen"
+        End If
+    End Sub
+
+    '-- Button designs --
 
     Private Sub btnSaveAsFile_MouseDown(sender As Object, e As MouseEventArgs) Handles btnSaveAsFile.MouseDown
         btnSaveAsFile.BackgroundImage = My.Resources.button_click
@@ -63,22 +92,5 @@
 
     Private Sub btnClose_MouseUp(sender As Object, e As MouseEventArgs) Handles btnClose.MouseUp
         btnClose.BackgroundImage = My.Resources.button
-    End Sub
-
-    Private Sub frmLog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        If My.Settings.Design = "Dark" Then
-            BackColor = Color.FromArgb(41, 41, 41)
-            lblHeader.ForeColor = Color.White
-            rtbLog.BackColor = Color.FromArgb(41, 41, 41)
-        End If
-
-        If My.Settings.Language = "German" Then
-            sfdLog.Title = "Speichere Log als Datei..."
-            btnSaveAsFile.Text = "Als Datei speichern"
-            btnClear.Text = "Leeren"
-            btnClose.Text = "Schließen"
-        End If
-
-        rtbLog.LoadFile(frmMain.AppData + "/Seeloewen Shutdown/DebugLogTemp")
     End Sub
 End Class
