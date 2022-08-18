a32bd1b0a369a7d9c87c9960dad8b3ae693dcd6c
Seeloewen <Louis.Herbert2006@gmail.com>
2022-08-18 04:14:49 +0200

    Add comments; Clean up code (frmChangelog)

--- Seeloewen Shutdown/frmChangelog.vb
+++ Seeloewen Shutdown/frmChangelog.vb
@@ -1,9 +1,19 @@
 ﻿Public Class frmChangelog
+
+    '-- Event handlers --
+
+    Private Sub frmChangelog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDesign()
+        LoadLanguage()
+    End Sub
+
     Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Close()
     End Sub
 
-    Private Sub frmChangelog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '-- Custom methods --
+
+    Private Sub LoadDesign()
         If My.Settings.Design = "Dark" Then
             BackColor = Color.FromArgb(41, 41, 41)
             rtbchangelogEnglish.BackColor = Color.FromArgb(41, 41, 41)
@@ -11,7 +21,9 @@
             lblChangelogDescription.ForeColor = Color.White
             lblChangelogHeader.ForeColor = Color.White
         End If
+    End Sub
 
+    Private Sub LoadLanguage()
         If My.Settings.Language = "German" Then
             lblChangelogDescription.Text = "Im Changelog können die Neuigkeiten von Versionen einfach abgerufen werden."
             rtbchangelogEnglish.Text = rtbChangelog.Text
